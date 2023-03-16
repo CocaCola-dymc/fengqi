f7f0660cf78bfb69c3a006b375e1c84412290899
CocaCola-dymc <1154679845@qq.com>
2023-03-16 22:38:39 +0800

    完成小程序前台与java后台的交互

--- wxService/src/pages/index/index.vue
+++ wxService/src/pages/index/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="wrapper">
-    <button @click="clickbutton">刷卡</button>
+    <button @click="doorOpen">开门</button>
+    <van-button type="primary" @click="queryUser">查询</van-button>
   </div>
 </template>
 
@@ -10,6 +11,7 @@ import { connect } from "mqtt";
 const mqttHost = "106.55.60.213"
 const mqttPort = "8083";
 const mqttUrl = `wx://${mqttHost}:${mqttPort}/mqtt`;
+const baseUrl = 'http://127.0.0.1:8080'
 
 const publish_Topic = "door_topic";
 const subscribe_Topic = "card_topic";
@@ -17,20 +19,44 @@ const subscribe_Topic = "card_topic";
 export default {
   data () {
     return {
-      client:{}
+      client:{},
+      info:{}
     };
   },
 
-  components: {
-  },
+  components: {},
 
   methods: {
-    clickbutton(){
+    doorOpen(){
       var that = this;
       console.log('开门')
       that.client.publish(publish_Topic,"1",function(err){
         if(!err){
           console.log("门已开启")
+        }
+      })  
+    },
+
+    queryUser(){
+      console.log("查询")
+      wx.request({
+        url: baseUrl+'/user/doFindUser',
+        method: "GET",
+        // data: {
+        //   username: username,
+        //   password: password,
+        //   createdTime: createdTime
+        // },
+        //成功回调函数
+        success: (res) => {
+          let data = res.data
+          console.log(data)
+          // //获取返回值列表
+          // list = res.data.data
+          // //返回值设置至data
+          // this.setData({
+          //   list: list
+          // })
         }
       })
     }
--- wxService/src/pages/index/main.js
+++ wxService/src/pages/index/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import App from './index'
+// import axios from 'axios'
 
-// add this to handle exception
-Vue.config.errorHandler = function (err) {
-  if (console && console.error) {
-    console.error(err)
-  }
-}
+// //导入Mint UI
+// import MintUI from 'mint-ui'
+// //导入样式文件
+// import 'mint-ui/lib/style.css'
+// //注册为Vue的插件
+// Vue.use(MintUI);
+
+
+Vue.config.productionTip = false
+// axios.defaults.baseURL = 'http://127.0.0.1:8080'
+// Vue.prototype.axios = axios
 
 const app = new Vue(App)
 app.$mount()
